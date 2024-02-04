f127f764ca81b48a3f51d76f51bd576c20aba95f
Sochima-Aristotle <ezeobisochima@gmail.com>
2024-02-04 12:56:30 -0800

    deployment stage

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,5 @@
-import { isDisabled } from '@testing-library/user-event/dist/utils'
-import React, {useState, useContext, useEffect} from 'react'
+// import { isDisabled } from '@testing-library/user-event/dist/utils'
+import React, { useState, useContext, useEffect } from 'react'
 import RatingSelect from './RatingSelect'
 import Button from './shared/Button'
 import Card from './shared/Card'
@@ -8,65 +8,65 @@ import FeedbackContext from './context/FeedbackContext'
 
 
 function FeedbackForm() {
-     const [text, setText] = useState('')
-     const [rating, setRating] = useState(10)
-     const [btnDisabled, setBtnDisabled] = useState(true)
-     const [message, setMessage] = useState('')
+  const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
 
-     const {addFeedBack, feedbackEdit, updateFeedback} = useContext(FeedbackContext)
+  const { addFeedBack, feedbackEdit, updateFeedback } = useContext(FeedbackContext)
 
-      useEffect(()=>{
+  useEffect(() => {
 
-       if(feedbackEdit.edit=== true){
-         setBtnDisabled(false);
-         setText(feedbackEdit.item.text)
-         setRating(feedbackEdit.item.rating)
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false);
+      setText(feedbackEdit.item.text)
+      setRating(feedbackEdit.item.rating)
 
-       }
-      }, [feedbackEdit])
-
-    const handleText = e=>{
-        e.preventDefault()
-      setText(e.target.value);
-      if(text ===''){
-        setBtnDisabled(true)
-        setMessage(null)
-      }else if(text !== '' && text.trim().length <=10){
-        setMessage('Text must be atleast 10 characters')
-        setBtnDisabled(true)
-      }else{
-        setMessage(null)
-        setBtnDisabled(false)
-      }
     }
+  }, [feedbackEdit])
 
-    const handleSubmit = e =>{
-      e.preventDefault()
-      if(text.trim().length >10){
-        const newFeedback ={
-          text,
-          rating
-        }
-
-        if(feedbackEdit.edit === true){
-          updateFeedback(feedbackEdit.item.id, newFeedback)
-        }else{
-          
-          addFeedBack(newFeedback)
-        }
-        setText('')
-      }
+  const handleText = e => {
+    e.preventDefault()
+    setText(e.target.value);
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (text !== '' && text.trim().length <= 10) {
+      setMessage('Text must be atleast 10 characters')
+      setBtnDisabled(true)
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
     }
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating
+      }
+
+      if (feedbackEdit.edit === true) {
+        updateFeedback(feedbackEdit.item.id, newFeedback)
+      } else {
+
+        addFeedBack(newFeedback)
+      }
+      setText('')
+    }
+  }
 
   return (
     <Card reverse={false} >
       <form onSubmit={handleSubmit}>
 
-        <h2 style={{paddingBottom: '0.7rem'}}>How would you rate our service with you?</h2>
-        <RatingSelect select={(rating)=> setRating(rating)}/>
+        <h2 style={{ paddingBottom: '0.7rem' }}>How would you rate our service with you?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
-            <input onChange={handleText} type="text" value={text} placeholder='Please write a review..'/>
-           <Button type="submit" isDisabled={btnDisabled}>Send</Button>
+          <input onChange={handleText} type="text" value={text} placeholder='Please write a review..' />
+          <Button type="submit" isDisabled={btnDisabled}>Send</Button>
         </div>
         {message && <div className='message'>{message}</div>}
       </form>
--- src/components/Feedbackitem.jsx
+++ src/components/Feedbackitem.jsx
@@ -1,25 +1,24 @@
-import {FaTimes, FaEdit} from 'react-icons/fa'
-import React, {useContext} from "react"
+import { FaTimes, FaEdit } from 'react-icons/fa'
+import React, { useContext } from "react"
 import Card from "./shared/Card"
 import FeedbackContext from './context/FeedbackContext'
 
 function Feedback({ item }) {
-const { deleteFeedback, editFeedback } = useContext(FeedbackContext)
-  // const handleClick =()=>{}
-   
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext)
+
   return (
     <Card>
-        <div className="num-display">{item.rating}</div>
-        <button onClick={()=> deleteFeedback(item.id)} className="close">
-          <FaTimes  color='white'/>
-        </button>
-        <button className='edit' onClick={()=> editFeedback(item)}>
-            <FaEdit color ='white' />
-        </button>
-        
-        <div className="text-display">
-            {item.text}
-        </div>
+      <div className="num-display">{item.rating}</div>
+      <button onClick={() => deleteFeedback(item.id)} className="close">
+        <FaTimes color='white' />
+      </button>
+      <button className='edit' onClick={() => editFeedback(item)}>
+        <FaEdit color='white' />
+      </button>
+
+      <div className="text-display">
+        {item.text}
+      </div>
     </Card>
   )
 }
